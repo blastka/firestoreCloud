5862984f62f35d8b0ea2b3ba138b136cbb9ba62a
blastka505 <necturna505@gmail.com>
2022-08-30 10:56:02 +0300

    Добавлен viewBinding

--- app/src/main/java/com/example/firebasefirestore/present/presents/MainActivity.kt
+++ app/src/main/java/com/example/firebasefirestore/present/presents/MainActivity.kt
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.example.firebasefirestore.R
+import com.example.firebasefirestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainActivityViewModel
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         val viewModelFactory = MainActivityFactory(applicationContext)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
     }
